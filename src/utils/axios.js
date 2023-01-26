1ff72394167145a90cc1fb86ea232ff53409deda
james <ali_o@felixin.io>
2023-01-26 21:22:17 +0330

    update base url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -17,7 +17,7 @@ const logError = (err) => {
   }
 }
 
-const API_URL = "http://192.168.43.247:8000"
+const API_URL = "https://api.ashraf-real-estate.ir"
 const API_BASE_PATH = "/api/v1/"
 
 const instance = axios.create({
